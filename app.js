f483b1f24c36115c830f0235c979096e248345e2
Bambi7NRV <test.svt36@gmail.com>
2021-11-03 14:13:51 +0100

    pdf

--- app.js
+++ app.js
@@ -5,15 +5,12 @@ const app = express()
 const port = 3000
 app.use(express.static('public'))
 
-
 app
     .set('views', 'templates')
     .set('twig options', { autoescape: true })
     .set('views engine', 'html')
     //.engine('html', twig.__express);
 
-
-var data =fs.readFileSync('./public/media/IntroNetKit.pdf','utf8') 
 app.get('/', (req, res) => {
     res.render('test.html.twig', {
         'nom' : "toto",
@@ -23,14 +20,12 @@ app.get('/', (req, res) => {
         ]
     })
 })
-
+var data =fs.readFileSync('./public/media/PetitTheoRR#4.pdf','utf8') 
 app.get('/pdf', (req, res) => {
-    res.render('pdf.html.twig', {
-        'nom' : "test",
+    res.render('pdf.html.twig', { 
+        'num' : res.sendFile(__dirname + "/public/media/PetitTheoRR#4.pdf")
     })
 })
-     
-
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`)
 })
