db83aa8befbb1ad713596ec6992158ee8ef8cc52
Bambi7NRV <test.svt36@gmail.com>
2021-11-04 13:48:27 +0100

    commit avant pull

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const app = express()
 const port = 3000
 app.use(express.static('public'))
 
+
 app
     .set('views', 'templates')
     .set('twig options', { autoescape: true })
     .set('views engine', 'html')
     //.engine('html', twig.__express);
-
+    
 app.get('/', (req, res) => {
     res.render('test.html.twig', {
         'nom' : "toto",
@@ -20,12 +21,19 @@ app.get('/', (req, res) => {
         ]
     })
 })
-var data =fs.readFileSync('./public/media/PetitTheoRR#4.pdf','utf8') 
-app.get('/pdf', (req, res) => {
-    res.render('pdf.html.twig', { 
-        'num' : res.sendFile(__dirname + "/public/media/PetitTheoRR#4.pdf")
-    })
-})
+
+//var data =fs.readFileSync('./public/media/PetitTheoRR#4.pdf','utf8') 
+//app.get('/pdf', (req, res) => {
+  //  res.render('pdf.html.twig', { 
+    //    'num' : res.sendFile(__dirname + "/public/media/PetitTheoRR#4.pdf")
+    //})
+//})
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`)
 })
+
+
+
+
+
+
