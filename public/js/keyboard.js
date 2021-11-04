ebba9ae61359b6285513aeb9a08c22c77796db3f
deliasTheo <t.delias22@gmail.com>
2021-11-04 23:23:56 +0100

    Ajout du clavier dans le home grace a valou

--- public/js/keyboard.js
+++ public/js/keyboard.js
@@ -1,21 +1,24 @@
+
 $(function(){
 	var $write = $('#write'),
 		shift = false,
 		capslock = false;
-	
+
 	$('#keyboard li').click(function(){
 		var $this = $(this),
 			character = $this.html();
-		
+
 		// Delete
 		if ($this.hasClass('delete')) {
 			var html = $write.html();
-			
+
 			$write.html(html.substr(0, html.length - 1));
 			return false;
 		}
-		
+
 		// Add the character
 		$write.html($write.html() + character);
 	});
 });
+
+
